3165662d2e80ec5f1500f459eb9a193de392b733
Milan Rybar <kontakt@milanrybar.cz>
2020-12-18 09:01:07 +0000

    Comment testing

--- nipy/__init__.py
+++ nipy/__init__.py
@@ -9,23 +9,23 @@ from .info import (LONG_DESCRIPTION as __doc__,
                    STATUS as __status__,
                    __version__)
 
-from nipy.testing import Tester
-test = Tester().test
-bench = Tester().bench
-
-
-def _test_local_install():
-    """ Warn the user that running with nipy being
-        imported locally is a bad idea.
-    """
-    import os
-    if os.getcwd() == os.sep.join(
-                            os.path.abspath(__file__).split(os.sep)[:-2]):
-        import warnings
-        warnings.warn('Running the tests from the install directory may '
-                     'trigger some failures')
-
-_test_local_install()
+# from nipy.testing import Tester
+# test = Tester().test
+# bench = Tester().bench
+# 
+# 
+# def _test_local_install():
+#     """ Warn the user that running with nipy being
+#         imported locally is a bad idea.
+#     """
+#     import os
+#     if os.getcwd() == os.sep.join(
+#                             os.path.abspath(__file__).split(os.sep)[:-2]):
+#         import warnings
+#         warnings.warn('Running the tests from the install directory may '
+#                      'trigger some failures')
+# 
+# _test_local_install()
 
 
 # Add to top-level namespace
@@ -37,7 +37,7 @@ from .pkg_info import get_pkg_info as _get_pkg_info
 get_info = lambda : _get_pkg_info(os.path.dirname(__file__))
 
 # Cleanup namespace
-del _test_local_install
+# del _test_local_install
 # If this file is exec after being imported, the following lines will
 # fail
 try:
